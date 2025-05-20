cfc11099cf3223e63f6a79c507c498366e04d8c3
luisdelmoral <luisgardelmoral@gmail.com>
2025-05-20 18:01:45 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to React</h1>
+    <h2>dea nada</h2>
+    </div>
+  );
+}
+
+export default App;
