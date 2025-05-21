375dcc93b1e09587246fb3acf4c74f107bf6563c
luisdelmoral <luisgardelmoral@gmail.com>
2025-05-21 11:43:58 +0200

    añadido menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
 import React from 'react';
+import './css/menuPrincipal.css'
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to React</h1>
-    <h2>dea nada</h2>
+      <MenuPrincipal />
     </div>
   );
+
 }
+function MenuPrincipal() {
+  return (
+    <ul className='menuPrincipal'>
+      <ListaArticulos />
+      <ListaMarcas />
+      <ListaProveedores />
+      <ListaContactos />
+    </ul>
+  );
+}
+
+function ListaArticulos() {
+  return (
+    <li><a>Artículos</a></li>
+  );
+}
+
+function ListaMarcas() {
+  return (
+    <li><a>Marcas</a></li>
+  );
+}
+
+function ListaProveedores() {
+  return (
+    <li><a>Proveedores</a></li>
+  );
+}
+
+function ListaContactos() {
+  return (
+    <li><a>Contactos</a></li>
+  );
+}
+
 
 export default App;
