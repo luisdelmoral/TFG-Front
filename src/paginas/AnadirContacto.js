4cd24099fb228c9cc2ca2534263d72da74cfe171
mgarmuno <miguelgarmm@gmail.com>
2025-05-23 17:56:30 +0200

    prueba contacto

--- src/paginas/AnadirContacto.js
+++ src/paginas/AnadirContacto.js
@@ -5,48 +5,38 @@ import { render } from '@testing-library/react';
 
 
 
-class AnadirContacto extends Component{
-    
-    state = {
-        nombre: '',
-        apellidos: '',
-        email: '',
-        telefono: 0 
-    };
+function AnadirContacto() {     
+    function submitCredentials(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        let data = {};
+        formData.forEach((value, key) => data[key] = value);
 
-    handleChange = (event) => {
-        this.setState({ [event.target.name]: event.target.value });
-    };
+        fetch("/api/contacto/anadir", {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+        })
+        .then(() => console.log("OK!"))
+        .catch(error => console.error(error));
+    }
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-        const formData = {
-            nombre: this.contacto.nombre,
-            apellidos: this.contacto.apellidos,
-            email: this.contacto.email,
-            telefono: this.contacto.telefono
-        }
-        axios.post('http://localhost:8080/api/contacto/anadir', {data: formData})
-        .then(response => console.log(response.data))
-        .then(error => console.error(error));
-    };
-
-    render() {
-        return(
-    <div>
-        <>
-        <form onSubmit={handleSubmit}>
-            <input type='text' name='nombre' value={this.contacto.nombre} onChange={this.handleChange}></input>
-            <input type='text' name='apellidos' value={this.contacto.apellidos} onChange={this.handleChange}></input>
-            <input type='text' name='email' value={this.contacto.email} onChange={this.handleChange}></input>
-            <input type='number' name='telefono' value={this.contacto.telefono} onChange={this.handleChange}></input>
-        </form>
-        </>
-        
-    </div>
+    return(
+        <div>
+            <>
+            <form onSubmit={submitCredentials}>
+                <input type='text' name='nombre' value={this.contacto.nombre} onChange={this.handleChange}></input>
+                <input type='text' name='apellidos' value={this.contacto.apellidos} onChange={this.handleChange}></input>
+                <input type='text' name='email' value={this.contacto.email} onChange={this.handleChange}></input>
+                <input type='number' name='telefono' value={this.contacto.telefono} onChange={this.handleChange}></input>
+            </form>
+            </>
+            
+        </div>
 
     );
-    }
 }
 
 export default AnadirContacto;
